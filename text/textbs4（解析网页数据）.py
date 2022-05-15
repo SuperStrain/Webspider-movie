d628e523a227a8842a3c658b5712c26e269efe32
SuperStrain <1143124497@qq.com>
2022-05-15 23:38:13 +0800

    .

--- text/textbs4（解析网页数据）.py
+++ text/textbs4（解析网页数据）.py
@@ -5,14 +5,18 @@
 # @Software: PyCharm
 
 # BeautifulSoup4将一个复杂的HTML文档转换成一个复杂的树形结构，每个节点都是Python对象，所有对象可以归纳为4种：
-# -Tag
-# -NavigableString
-# -BeautifulSoup
-# -Comment
+# 1、-Tag
+# 2、-NavigableString
+# 3、-BeautifulSoup
+# 4、-Comment
 from bs4 import BeautifulSoup
 file=open("./百度.html","rb")
 html=file.read()
 bs=BeautifulSoup(html,"html.parser")
-# print(bs.title)
-# print(bs.head)
-# print(bs.a)
+
+#1、-Tag 标签及其内容：拿到他所找到的第一个内容
+print(bs.title)
+print(type(bs.title))
+#2、-NavigableString
+print(bs.title)
+print(type(bs.title))
