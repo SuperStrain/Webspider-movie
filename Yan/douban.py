165cb62b9b53d4b52824b074f8b41573dd315802
SuperStrain <1143124497@qq.com>
2022-05-15 12:02:19 +0800

    首次爬虫，探究urllib

---
+++ Yan/douban.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup #网页解析
+import re   #正则表达式，进行文字匹配
+import urllib.request,urllib.error  #制定URL
+import xlwt     #进行Excel操作
+import sqlite3  #进行SQLite数据库操作
+
+def main():
+    baseurl="https://movie.douban.com/top250?start="
+    # 1、爬取网页
+    datalist=getData(baseurl)
+    savepath=".\\豆瓣电影Top250.xls"
+    # 3、保存数据
+    saveData(savepath)
+
+def getData(baseurl):
+    datalist=[]
+    # 2、逐一解析数据
+    return datalist
+
+# 保存数据
+def saveData(savepath):
+    print(" ")
+
+if __name__=='__main__':
+    main()
---
+++ text/textUrllib.py
@@ -0,0 +1,26 @@
+
+import urllib.request
+#获取一个get请求
+# response=urllib.request.urlopen("http://www.baidu.com")
+# print(response.read().decode('utf-8'))#对获取到的网页进行utf-8解码（防止中文乱码）
+
+#使用httpbin.org进行如下请求（主要是post请求借助该网站）
+
+#get请求,并为超时做一个计划性的异常处理
+# try:
+#     response=urllib.request.urlopen("http://httpbin.org/get",timeout=0.01)
+#     print(response.read().decode('utf-8'))
+# except urllib.error.URLError as e:
+#     print(e)
+#     print("超出预计时间!!")
+
+# 获取一个post请求:用于模拟用户真实登录时使用
+import urllib.parse #解析
+data=bytes(urllib.parse.urlencode({"hello":"world"}),encoding="utf-8") # 提交一个表单信息才能访问（可以是用户名、密码、cookie等）
+response=urllib.request.urlopen("http://httpbin.org/post",data=data)
+print(response.read().decode('utf-8'))
+
+
+# response=urllib.request.urlopen("http://www.baidu.com")
+# # print(response.status)
+# print(response.getheaders())
