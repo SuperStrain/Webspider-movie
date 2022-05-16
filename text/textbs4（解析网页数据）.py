04ddcfdfb4fd103aa69d715ff3f65cabc45b90b7
SuperStrain <1143124497@qq.com>
2022-05-16 11:28:43 +0800

    5月16日 BeautifulSoup补充结束

--- text/textbs4（解析网页数据）.py
+++ text/textbs4（解析网页数据）.py
@@ -36,20 +36,51 @@ bs=BeautifulSoup(html,"html.parser")
 #文档的搜索(主要)
 
 #（1）find_all() 字符串过滤，查找与字符串完全相同的内容
-# t_list=bs.find_all("a")
-# print(t_list)
+# t_list=bs.find_all("a") #带<a>的
+# for item in t_list:
+#     print(item)
 
-#正则表达式搜索 使用search()的方法来查找内容,只要有'a'即可
-# import re
+#正则表达式搜索 使用search()的方法来查找内容,只要有元素'a'即可
+import re
 # t_list=bs.find_all(re.compile("a"))
-# print(t_list)
+# for item in t_list:
+#     print(item)
 
 #传入有个函数（方法），根据函数的方法来搜索
-def name_is_exists(tag):
-    return tag.has_attr("name")
+# def name_is_exists(tag):
+#     return tag.has_attr("name")
+#
+# t_list=bs.find_all(name_is_exists)
+# for item in t_list:
+#     print(item)
 
-t_list=bs.find_all(name_is_exists)
-for item in t_list:
-    print(item)
+#（2）kwargs    参数
+# t_list=bs.find_all(id="head")
+# t_list=bs.find_all(href="http://news.baidu.com")
+# for item in t_list:
+#     print(item)
 
-#（2）
+# (3)text  参数
+# t_list=bs.find_all(text=["hao123","地图"])
+# t_list=bs.find_all(text=re.compile("\d"))#应用正则表达式来查找包含特定文本的内容（标签里的字符串）
+# for item in t_list:
+#     print(item)
+
+#（4）limit 参数 限制搜索数量
+# t_list=bs.find_all("a")
+# t_list=bs.find_all("a",limit=3)
+# for item in t_list:
+#     print(item)
+
+
+#选择器 css
+# t_list=bs.select('title')  #通过标签来查找
+# t_list=bs.select(".mnav") #通过类名来查找
+# t_list=bs.select("#u1") #通过id来查找
+# t_list=bs.select("a[class='bri']")#通过属性来查找
+# t_list=bs.select("head > title")#通过子标签来查找
+# for item in t_list:
+#     print(item)
+
+# t_list=bs.select(".mnav ~ .bri")#查找在mnav类别里的bri类别
+# print(t_list[0].get_text())#获取第一个元素文本
